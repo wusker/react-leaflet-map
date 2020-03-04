170054e69080de52fe53d2a9cd42894aa4e2b587
wusker <s.bergellio@gmail.com>
2020-03-03 23:26:28 -0700

    Map showing with marker on user location.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import L from 'leaflet';
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
+
+import userLocationURL from './userLocation.svg';
 import './App.css';
 
+const myIcon = L.icon({
+  iconUrl: userLocationURL,
+  iconSize: [50, 82]
+});
+
 function App() {
+  const [position, setPosition] = useState({
+    lat: 45,
+    lng: -112
+  });
+  const [zoom, setZoom] = useState(2);
+  let [access, setAccess] = useState(false);
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(pos => {
+      const { latitude, longitude } = pos.coords;
+      setPosition({
+        lat: latitude.toFixed(1),
+        lng: longitude.toFixed(1)
+      });
+      setZoom(5);
+      setAccess(true);
+    });
+  }, [position]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Map className="map" center={position} zoom={zoom} >
+      <TileLayer
+        attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      {
+        access ?
+          <Marker position={position} icon={myIcon}>
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker> : ''
+      }
+    </Map>
   );
 }
 
